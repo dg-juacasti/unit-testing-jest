0a77feb00c3eac2e65920e8b19bfb2a06dfa9ced
Javier Simbana <wsimbana@pichincha.com>
2022-08-16 12:19:44 -0500

    JSI: completing tests

--- src/calculator.js
+++ src/calculator.js
@@ -1,19 +1,23 @@
 const calculator = (operator, number1, number2) => {
-  if (isNaN(number1) || isNaN(number2)) {
-    return "Values have to be numbers"
-  }
-  switch (operator) {
-    case "+":
-      return number1 + number2;
-    case "-":
-      return number1 - number2;
-    case "*":
-      return number1 * number2;
-    case "/":
-      return number1 / number2;
-    default:
-      return "Operator not found"
-  }
+
+
+    if (operator === undefined) return
+    if (isNaN(number1) || isNaN(number2)) {
+      throw new Error("Values have to be numbers")
+    }
+    switch (operator) {
+      case "+":
+        return number1 + number2;
+      case "-":
+        return number1 - number2;
+      case "*":
+        return number1 * number2;
+      case "/":
+        return number1 / number2;
+      default:
+        return "Operator not found"
+    }
+
 };
 
-exports.calculator = calculator;
+module.exports = { calculator } 
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,13 +1,41 @@
-const { calculator } = require("./calculator");
+const { calculator } = require('./calculator') // Arrange
 
-describe('Test over calculator', () => { 
-  it("2 + 2 should be 4", () => {
-    expect(calculator("+", 2, 2)).toBe(4);
+describe('Test over calculator', () => {
+
+  test("2 + 2 should be 4", () => {
+    const result = calculator("+", 2, 2) //Act
+    expect(result).toBe(4); // Assert
   });
+
   it("-2 + 2 should be 0", () => {
-    expect(calculator("+", -2, 2)).toBe(0);
+    const result = calculator("+", -2, 2)
+    expect(result).toBe(0);
   });
+
   it("-2 + -7 should be 9", () => {
-    expect(calculator("+", -2, -7)).toBe(-9);
+    const result = calculator("+", -2, -7)
+    expect(result).toBe(-9);
   });
+
+  it("-2 - -7 should be 9", () => {
+    const result = calculator("-", -2, -7)
+    expect(result).toBeGreaterThan(4)
+  });
+
+  it("10 / 3 should be close to 3.33 with 5 decimals", () => {
+    const result = calculator("/", 10, 3)
+    expect(result).toBeCloseTo(3.33333, 5)
+  })
+
+  it("should return undefined", () => {
+    const result = calculator(undefined, 2, 2)
+    expect(result).toBeUndefined()
+  })
+
+  it("Should return an error", () => {
+    const result = () => calculator("+", 'f', 'a')
+    expect(result).toThrow(/Values/)
+  })
+
+
 })
